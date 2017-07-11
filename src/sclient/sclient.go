d48192edc9abc2085b65c0ecdc0f5a110f67969f
Walt Drummond <walt@drummond.us>
2017-07-11 14:02:29 -0700

    ssh client tests

--- src/sclient/sclient.go
+++ src/sclient/sclient.go
@@ -1,25 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"golang.org/x/crypto/ssh"
-	"time"
-	"io/ioutil"
 )
 
 var remote string = "127.0.0.1:22"
 
 func main() {
-//	var hostKey ssh.PublicKey
 	config := &ssh.ClientConfig{
-		User: "wd",
+		User: "walt",
 		Auth: []ssh.AuthMethod{
-			// Use the PublicKeys method for remote authentication.
-			ssh.PublicKeys(signer),
+			ssh.Password("ks120872"),
 		},
-		//		HostKeyCallback: ssh.FixedHostKey(hostKey)
-		HostKeyCallback: ssh.InsecureHostKey(), // Danger?
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // Danger?
 	}
 
 	log.Printf("SSH'inh to %s", remote)
@@ -27,15 +21,46 @@ func main() {
 	if err != nil {
 		log.Fatalf("Dial(): %s", err)
 	}
+	log.Printf("Made a connection\n")
+	defer client.Close()
 
+	channel, _, err := client.Conn.OpenChannel("session", nil)
+	if err != nil {
+		log.Fatal("OpenChannel", err)
+	}
+	channel.Write([]byte("Hello!"))
+
+
+/*
+	// Create a session
 	session, err := client.NewSession()
 	if err != nil {
-		log.Fatalf("NewSession(): %s", err)
+    		log.Fatal("unable to create session: ", err)
 	}
+	defer session.Close()
 
-	for i := 0; i < 100; i++ {
-		fmt.Fprintf(session.Stdout, "Hello!\n")
-		time.Sleep(250 * time.Millisecond)
+	// Set up terminal modes
+	modes := ssh.TerminalModes{
+    		ssh.ECHO:          0,     // disable echoing
+    		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+    		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}	
+	// Request pseudo terminal
+	if err := session.RequestPty("xterm", 40, 80, modes); err != nil {
+    		log.Fatal("request for pseudo terminal failed: ", err)
 	}
-	session.Close()
+	// Start remote shell
+
+	send, err :=  session.StdinPipe()
+	if err != nil {
+		log.Fatal("StdoutPipe(): ", err)
+	}
+	send.Write([]byte("Hello!"))
+*/
+/*
+	if err := session.Shell(); err != nil {
+    		log.Fatal("failed to start shell: ", err)
+	}	
+	log.Printf("Shell() returned\n")
+*/
 }
