3c1e55d3c00e971324afc69791b3a284f4157d05
Walt Drummond <walt@drummond.us>
2017-10-27 13:38:42 -0700

    Move word wrap function

---
+++ src/hayes/utils.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"strings"
+)
+
+// Borrowed from https://gist.github.com/kennwhite/306317d81ab4a885a965e25aa835b8ef
+func lineWrap(text string, lineWidth int) string {
+	words := strings.Fields(strings.TrimSpace(text))
+	if len(words) == 0 {
+		return text
+	}
+	wrapped := words[0]
+	spaceLeft := lineWidth - len(wrapped)
+	for _, word := range words[1:] {
+		if len(word)+1 > spaceLeft {
+			wrapped += "\n" + word
+			spaceLeft = lineWidth - len(word)
+		} else {
+			wrapped += " " + word
+			spaceLeft -= 1 + len(word)
+		}
+	}
+
+	return wrapped
+}
